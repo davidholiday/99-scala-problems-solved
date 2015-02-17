50f153806581af76173af9d5e34968bd43cdae18
David Holiday <davidh@annaisystems.com>
2015-02-17 15:52:43 -0800

    basic solution-file discovery

---
+++ src/main/resources/problemSolutions/sp_01.scala
@@ -0,0 +1,13 @@
+package main.resources.problemSolutions
+
+/**
+ * Created by snerd on 2/17/15.
+ */
+
+object p01 {
+
+  def solution(): Unit = {
+    println("in p01, baby!")
+  }
+
+}
--- src/main/scala/bootstrap.scala
+++ src/main/scala/bootstrap.scala
@@ -1,13 +1,58 @@
 /**
- * Created by snerd on 2/17/15.
+ * testrunner for 99 scala problem solutions.
+ *
+ * 99 problems found here:: http://aperiodic.net/phil/scala/s-99/
+ *
+ * FEB'15 David Holiday
  */
+
+import java.io.File
 
 object bootStrap {
 
     def main(args: Array[String]): Unit = {
-      println("foop!")
+
+      for (fName <- getFileList(new File("./src/main/resources"))) {
+        println(fName.getName )
+      }
+
+    }
+
+
+  /**
+   * trolls through working directory and all sub directories in search of files
+   * with names starting with "sp_" ('sp' for 'scala problem'). thanks to
+   * stack overflow for base code: http://stackoverflow.com/questions/2637643/
+   * how-do-i-list-all-files-in-a-subdirectory-in-scala
+   *
+   * @param f - java File object
+   * @return targetFileList - Array[java.io.File] - list of file objects
+   *         that have names starting with "sp_"
+   */
+    def getFileList(f: File): Array[File] = {
+      // get file list, filter that list by checking filename against regex
+      val targetFileList = f.listFiles().filter(_.getName().matches("^sp_.*"))
+
+      // get a list of subdirectories in current directory
+      val dirList = f.listFiles.filter(_.isDirectory)
+
+      for (dName <- dirList) {
+        println("searching for solutions in directory: " + dName)
+      }
+
+      targetFileList ++ f.listFiles.filter(_.isDirectory).flatMap(getFileList)
+    }
+
+
+    def getFileListTwo(f: File): Array[File] = {
+      val targetFileListAll
+
+
     }
 
 
 }
+
+
+
 
