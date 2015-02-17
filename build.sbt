06bf501ec542e08e8206235f3146372c67aff3d6
David Holiday <davidh@annaisystems.com>
2015-02-17 11:11:05 -0800

    hello world commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "99_Scala_problems"
+
+version := "1.0"
+
+scalaVersion := "2.11.5"
+    
---
+++ src/main/scala/bootstrap.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by snerd on 2/17/15.
+ */
+
+object bootStrap {
+
+    def main(args: Array[String]): Unit = {
+      println("foop!")
+    }
+
+
+}
+
