de96402f30d115127523001e02aa2424a51f1267
dholiday <davidholiday@hush.com>
2016-02-16 12:09:53 -0800

    added question solution from different set

---
+++ src/main/scala/solutions/everyvotecounts/Sp9999.scala
@@ -0,0 +1,86 @@
+package solutions.everyvotecounts
+
+import solutions._
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Question 1 - Anagrams
+ * Write a function to determine if two strings of unknown length (but not 
+ * longer than 100 MB each) are anagrams of each other, that is, contain 
+ * exactly the same number of each letter of the alphabet. It should be case 
+ * insensitive, should ignore all characters but the 26 letters of the English 
+ * alphabet, should be very time-efficient, and should be space-efficient. 
+ * Include a brief explanation of why and how it works efficiently. Include 
+ * tests if you have time.
+ * 
+ * 
+ * David Holiday
+ * 
+ * 
+ */
+class Sp9999 extends SpMeta {
+  
+  type inputT = List[(String, String)]
+  type solutionT = List[Boolean]
+
+  // valid anagram - tests lower case function
+  val input1a = "untidy"
+  val input1b = "nuDity"
+  
+  // valid anagram - tests rejection of non-alpha characters 
+  val input2a = "untidy@1"
+  val input2b = "nudity1"
+  
+  // invalid anagram
+  val input3a = "finklepuss78"
+  val input3b = "untidyuntidy"
+  
+  // input to be fed into 
+  val input = List((input1a, input2a), (input2a, input2b), (input3a, input3b));
+
+  
+  /*
+   * transmutes the input into the solution to the stated problem.
+   */
+  def getSolution: solutionT = {    
+    val solution = ListBuffer[Boolean]()
+    input.map(x => solution.append(compareStrings(x)))
+    return solution.toList;
+  }
+
+  
+  /*
+   * homogenizes a pair of strings then checks to see if they have the same
+   * number of symbol types by comparing their sums. 
+   */
+  def compareStrings(inputTuple:(String, String)): Boolean = {
+    
+	  val sum1 = inputTuple._1
+                         .par
+                         .map(x => x.toLower)
+			                   .filter {x => x.toString() matches "([a-z])"}
+	                       .sum
+
+
+	  val sum2 = inputTuple._2
+                         .par
+	                       .map(x => x.toLower)
+	                       .filter {x => x.toString() matches "([a-z])"}
+	                       .sum
+                         
+	  return (sum1.equals(sum2));
+  }
+
+  
+  /*
+   * ensures the computed solution is correct.
+   */
+  def checkSolution(solution: solutionT): Unit = {
+    assert(solution == List(true, true, false))
+  }
+  
+  
+   
+}
+
+
